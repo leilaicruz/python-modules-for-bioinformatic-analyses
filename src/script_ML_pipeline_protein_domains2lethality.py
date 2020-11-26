5b92de9e9e35093eef25d9ac3fcdb5e6e16d55de
Leila Iñigo de la Cruz <leilaicruz@users.noreply.github.com>
2020-11-26 13:24:55 +0100

    Update script_ML_pipeline_protein_domains2lethality.py

--- src/script_ML_pipeline_protein_domains2lethality.py
+++ src/script_ML_pipeline_protein_domains2lethality.py
@@ -131,7 +131,7 @@ b.set_title('How the std varies with Lethality')
 pair=sns.pairplot(corr_keys,hue='lethality',diag_kind='kde',kind='reg',palette='Paired')
 pair.fig.suptitle('Pairplot to see data dependencies with Lethality',y=1.08)
 
-plt.savefig('Lethality_correlation_with_mean_and_std.png',dpi=100,format='png')
+plt.savefig('Lethality_correlation_with_mean_and_std.png',dpi=100)
 # %% P- values and correlation
 
 a=scipy.stats.pearsonr(corr_keys['mean'],corr_keys['lethality'])
@@ -288,7 +288,7 @@ plt.tight_layout()
 plt.title('Confusion matrix', y=1.1)
 plt.ylabel('Actual label')
 plt.xlabel('Predicted label')
-plt.savefig("confusion_matrix.png",dpi=100,format='png')
+plt.savefig("confusion_matrix.png",dpi=100)
 
 # %% Evaluation of the classifier in terms of overfitting : Cross Validation!
 
